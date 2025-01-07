251c291c6a191278e9a5a329f2a30c7048f58e72
sunboy <qysk@vip.qq.com>
2025-01-07 23:37:19 +0800

    init

---
+++ src/view/home_style/wap_default/info.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name' => '默认WAP模板',
+    'version' => '1.0.0',
+    'type' => 'wap'
+];
---
+++ src/view/user_style/default/info.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'name' => '默认模板',
+    'version' => '1.0.0',
+    'type' => 'pc'
+];
