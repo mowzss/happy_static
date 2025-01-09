6eaac2cc0e6d44e97b7c7b58e202ef30e8a465bd
sunboy <qysk@vip.qq.com>
2025-01-09 21:31:58 +0800

    init

--- dist/view/555/default/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'name' => 'PC会员中心模板',
-    'desc' => '系统默认的会员中心模板',
-    'type' => 'pc',
-    'image' => '',
-    //'author' => 'yxd',
-];
--- src/view/555/wap_default/info.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'name' => '默认WAP模板',
-    'desc' => '系统默认的模板',
-    'type' => 'WAP',
-    'image' => '',
-    //'author' => 'yxd',
-];
