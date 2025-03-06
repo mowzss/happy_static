c13b217d711666780142c6dd7db56a975519af04
sunboy <qysk@vip.qq.com>
2025-03-06 23:19:31 +0800

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -120,7 +120,7 @@ function buildStaticHtml() {
 function buildTemplateHtml() {
     return gulp.src(paths.template.src.html)
         .pipe(changed(paths.template.dist))
-        // .pipe(cleanHtml())
+        .pipe(isProduction ? cleanHtml() : gulp.dest(paths.template.dist))
         .pipe(gulp.dest(paths.template.dist));
 }
 
